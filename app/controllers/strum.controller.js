d6e22a22d8a6d98e7d91b2e00e8067db34b202bd
David Pate <davidtpate@gmail.com>
2015-11-08 11:19:55 -0500

    Add listening for instruments changing

--- app/controllers/strum.controller.js
+++ app/controllers/strum.controller.js
@@ -7,32 +7,35 @@ angular.module('houseBand')
 
   this.message = "Instruments";
   this.choice = '';
-  this.room = $stateParams.room
-  var instrumentsUrl = window.apiConfig.roomUrl + self.room + '/instruments'
-
-  connectToRoom(this.room)
+  this.room = $stateParams.room;
+  var instrumentsUrl = window.apiConfig.roomUrl + self.room + '/instruments';
 
   // Initial GET to see if anyone else has selected an instrument
   $http.get(instrumentsUrl).then(function(data){
     self.available = data;
   });
 
+  connectToRoom(this.room);
+  window.io.on('instruments changed', function(data) {
+    self.available = data;
+  });
+
   // POST to the server to select an instrument
   this.instrumentChoice = function(instrument, e){
-    var target = angular.element(e.target)
-    target.toggleClass('selected').next().toggleClass('bounceIn')
+    var target = angular.element(e.target);
+    target.toggleClass('selected').next().toggleClass('bounceIn');
     $http.post(instrumentsUrl + '/' + instrument).then(function(data){
       console.log(data)
     });
     self.choice = target.text().toLowerCase();
-  }
+  };
 
   this.goToInstrument = function(instrument){
     if(instrument !== ''){
       $state.go(instrument)
     }
     else {
-      alert('Oops, Please select an instrument')
+      alert('Oops, Please select an instrument');
       return false;
     }
   };
